eaca268a333bb4aca08f84c7fceaab9c2a16b7a2
Toshit Chawda <r58playz@gmail.com>
2024-06-20 15:11:06 -0700

    segmented buttons

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,19 +1,46 @@
-import { Card, Button, ButtonLink, Switch, CardClickable } from 'm3-dreamland';
+import { Icon, Card, Button, ButtonLink, Switch, CardClickable, SegmentedButtonContainer, SegmentedButtonItem } from 'm3-dreamland';
+import iconCode from '@ktibow/iconset-material-symbols/code';
+import iconTriangle from '@ktibow/iconset-material-symbols/change-history-outline';
+import iconSquare from '@ktibow/iconset-material-symbols/square-outline';
+import iconCircle from '@ktibow/iconset-material-symbols/circle-outline';
+
+const SegmentedPlayground = function() {
+	if (this.multiSelect) {
+		return (
+			<div>
+				<SegmentedButtonContainer>
+					<SegmentedButtonItem type="checkbox" input="segmented-a-0" checked={true}>$</SegmentedButtonItem>
+					<SegmentedButtonItem type="checkbox" input="segmented-a-1">$$</SegmentedButtonItem>
+					<SegmentedButtonItem type="checkbox" input="segmented-a-2" disabled={true}>$$$</SegmentedButtonItem>
+				</SegmentedButtonContainer>
+			</div>
+		)
+	} else {
+		return (
+			<div>
+				<SegmentedButtonContainer>
+					<SegmentedButtonItem name="segmented-b" input="segmented-b-0" icon={iconTriangle} checked={true}>Tab A</SegmentedButtonItem>
+					<SegmentedButtonItem name="segmented-b" input="segmented-b-1" icon={iconSquare}>Tab B</SegmentedButtonItem>
+					<SegmentedButtonItem name="segmented-b" input="segmented-b-2" icon={iconCircle} disabled={true}>Tab C</SegmentedButtonItem>
+				</SegmentedButtonContainer>
+			</div>
+		)
+	}
+}
 
 // javascript syntax for defining components
 const Home = function() {
 	this.counter = 0;
 
 	this.css = `
-		display: grid;
-		grid-auto-rows: 16em;
-		grid-template-columns: repeat(4, 1fr);
+		display: flex;
+		flex-direction: column;
 		gap: 1em;
 
 		.buttons {
 			display: flex;
 			flex-direction: row;
-			row-gap: 1em;
+			column-gap: 1em;
 		}
 	`;
 
@@ -45,6 +72,10 @@ const Home = function() {
 			</Card>
 			<Card type="filled">
 				<p>
+					<ButtonLink type="elevated" iconType="left" href="https://dreamland.js.org"><Icon icon={iconCode} />Dreamland</ButtonLink>
+					<ButtonLink type="tonal" iconType="full" href="https://dreamland.js.org"><Icon icon={iconCode} /></ButtonLink>
+				</p>
+				<p>
 					<ButtonLink type="elevated" href="https://dreamland.js.org">Dreamland</ButtonLink>
 					<ButtonLink type="filled" href="https://dreamland.js.org">Dreamland</ButtonLink>
 					<ButtonLink type="tonal" href="https://dreamland.js.org">Dreamland</ButtonLink>
@@ -54,6 +85,12 @@ const Home = function() {
 			</Card>
 			<Card type="filled">
 				<Switch></Switch>
+			</Card>
+			<Card type="filled">
+				<SegmentedPlayground multiSelect={true} />
+			</Card>
+			<Card type="filled">
+				<SegmentedPlayground multiSelect={false} />
 			</Card>
 		</div>
 	);
