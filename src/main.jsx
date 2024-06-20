19b52584d336253f05c9e4b0ed0cfbdea705562c
Toshit Chawda <r58playz@gmail.com>
2024-06-19 22:10:58 -0700

    initial ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import 'dreamland/dev';
 import { Router } from './router.jsx';
-const App = function () {
-    this.mount = () => {
-        Router.render(this.root);
-    };
+import { Styles } from 'm3-dreamland';
 
-    return <div id="app" />;
+const App = function() {
+	this.mount = () => {
+		Router.render(this.renderRoot);
+	};
+
+	return (
+		<div id="app">
+			<Styles
+				light={{ "primary": 4287646528, "onPrimary": 4294967295, "primaryContainer": 4294957780, "onPrimaryContainer": 4281993477, "inversePrimary": 4294948008, "secondary": 4286010961, "onSecondary": 4294967295, "secondaryContainer": 4294957780, "onSecondaryContainer": 4281079057, "tertiary": 4285553710, "onTertiary": 4294967295, "tertiaryContainer": 4294696870, "onTertiaryContainer": 4280621568, "error": 4290386458, "onError": 4294967295, "errorContainer": 4294957782, "onErrorContainer": 4282449922, "background": 4294965494, "onBackground": 4280490264, "surface": 4294965494, "onSurface": 4280490264, "surfaceVariant": 4294303194, "onSurfaceVariant": 4283646785, "inverseSurface": 4281937452, "inverseOnSurface": 4294962666, "outline": 4286935920, "outlineVariant": 4292395710, "shadow": 4278190080, "scrim": 4278190080, "surfaceDim": 4293449427, "surfaceBright": 4294965494, "surfaceContainerLowest": 4294967295, "surfaceContainerLow": 4294963438, "surfaceContainer": 4294765287, "surfaceContainerHigh": 4294436065, "surfaceContainerHighest": 4294041564, "surfaceTint": 4287646528 }}
+				dark={{ "primary": 4294948008, "onPrimary": 4283833878, "primaryContainer": 4285740074, "onPrimaryContainer": 4294957780, "inversePrimary": 4287646528, "secondary": 4293377462, "onSecondary": 4282657061, "secondaryContainer": 4284301114, "onSecondaryContainer": 4294957780, "tertiary": 4292789388, "onTertiary": 4282265092, "tertiaryContainer": 4283843609, "onTertiaryContainer": 4294696870, "error": 4294948011, "onError": 4285071365, "errorContainer": 4287823882, "onErrorContainer": 4294957782, "background": 4279898384, "onBackground": 4294041564, "surface": 4279898384, "onSurface": 4294041564, "surfaceVariant": 4283646785, "onSurfaceVariant": 4292395710, "inverseSurface": 4294041564, "inverseOnSurface": 4281937452, "outline": 4288711817, "outlineVariant": 4283646785, "shadow": 4278190080, "scrim": 4278190080, "surfaceDim": 4279898384, "surfaceBright": 4282529589, "surfaceContainerLowest": 4279503883, "surfaceContainerLow": 4280490264, "surfaceContainer": 4280753436, "surfaceContainerHigh": 4281477158, "surfaceContainerHighest": 4282200624, "surfaceTint": 4294948008 }} extraStyles={true} />
+			<div bind:this={use(this.renderRoot)} />
+		</div>
+	);
 };
 
 window.addEventListener('load', () => {
-    document.getElementById('app').replaceWith(<App />);
+	document.getElementById('app').replaceWith(<App />);
 });
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,12 +1,59 @@
+import { Card, Button, ButtonLink, CardClickable } from 'm3-dreamland';
+
 // javascript syntax for defining components
-const Home = function () {
-    this.counter = 0;
-    return (
-        <div>
-            <button on:click={() => this.counter++}>Click me!</button>
-            <p>{use(this.counter)}</p>
-        </div>
-    );
+const Home = function() {
+	this.counter = 0;
+
+	this.css = `
+		display: grid;
+		grid-auto-rows: 16em;
+		grid-template-columns: repeat(4, 1fr);
+		gap: 1em;
+
+		.buttons {
+			display: flex;
+			flex-direction: row;
+			row-gap: 1em;
+		}
+	`;
+
+	return (
+		<div>
+			<Card type="elevated">
+				<p class="buttons">
+					<Button type="elevated" on:click={() => this.counter++}>Click me!</Button>
+					<Button type="filled" on:click={() => this.counter++}>Click me!</Button>
+					<Button type="tonal" on:click={() => this.counter++}>Click me!</Button>
+					<Button type="outlined" on:click={() => this.counter++}>Click me!</Button>
+					<Button type="text" on:click={() => this.counter++}>Click me!</Button>
+				</p>
+				<Card type="filled">
+					{use(this.counter)}
+				</Card>
+			</Card>
+			<Card type="outlined">
+				Hiii :3
+				<CardClickable type="elevated">
+					Hiii
+				</CardClickable>
+				<CardClickable type="filled">
+					Hiii
+				</CardClickable>
+				<CardClickable type="outlined">
+					Hiii
+				</CardClickable>
+			</Card>
+			<Card type="filled">
+				<p>
+					<ButtonLink type="elevated" href="https://dreamland.js.org">Dreamland</ButtonLink>
+					<ButtonLink type="filled" href="https://dreamland.js.org">Dreamland</ButtonLink>
+					<ButtonLink type="tonal" href="https://dreamland.js.org">Dreamland</ButtonLink>
+					<ButtonLink type="outlined" href="https://dreamland.js.org">Dreamland</ButtonLink>
+					<ButtonLink type="text" href="https://dreamland.js.org">Dreamland</ButtonLink>
+				</p>
+			</Card>
+		</div>
+	);
 };
 
 export default Home;
