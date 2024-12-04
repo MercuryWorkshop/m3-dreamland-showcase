3abcb41b079fdd744ade963cbec96a9a654b8e8f
Toshit Chawda <r58playz@gmail.com>
2024-12-03 17:42:58 -0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import 'dreamland';
-import { router } from './router.tsx';
 import { StyleFromParams } from 'm3-dreamland';
+import Home from './routes/home';
 
 const App: Component<{}, { renderRoot: HTMLElement }> = function() {
-	this.mount = () => {
-		router.mount(this.renderRoot);
-	};
-
 	return (
 		<div id="app">
 			<StyleFromParams scheme="tonal_spot" contrast={0} color="CBA6F7" />
-			<div bind:this={use(this.renderRoot)} />
+			<Home />
 		</div>
 	);
 };
--- src/router.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect, Route, Router } from 'dreamland-router';
-import Home from './routes/home.jsx';
-export const router = new Router(
-	<Route>
-		<Route path="/" show={<Home />} />
-	</Route>
-);
